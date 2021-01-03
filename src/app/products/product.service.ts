b6930dbe9ca67daa0f7d50888fb94ba79b85dea2
mokbhai <mokshitjain18@gmail.com>
2021-01-03 18:02:25 +0530

    addd some cat

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { from,Observable } from 'rxjs';
+import { Cat } from '../site-layout/cat';
 import { Product } from './product';
 @Injectable({
   providedIn: 'root'
@@ -9,27 +10,31 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
   createProduct(productBody:Observable<Product>){
-    const baseUrl = "http://localhost:4200/Products";
+    const baseUrl = "http://localhost:3000/Products";
     return this.httpClient.post<Product>(baseUrl, productBody);
   }
   viewProduct(productID:Observable<Product>){
-    const baseUrl = "http://localhost:4200/Products" + productID;
+    const baseUrl = "http://localhost:3000/Products" + productID;
     return this.httpClient.get<Product>(baseUrl);
   }
   updateProduct(productBody:Observable<Product>, productID:Observable<Product>) {
-    const baseUrl = "http://localhost:4200/Products" + productID;
+    const baseUrl = "http://localhost:3000/Products" + productID;
     return this.httpClient.put<Product>(baseUrl, productBody);
   }
   deleteProduct(productID:Observable<Product>) {
-    const baseUrl = "http://localhost:4200/Products" + productID;
+    const baseUrl = "http://localhost:3000/Products" + productID;
     return this.httpClient.delete<Product>(baseUrl);
   }
   serchCatProduct(catID:Observable<Product>) {
-    const baseUrl = "http://localhost:4200/cat=" + catID;
+    const baseUrl = "http://localhost:3000/cat=" + catID;
     return this.httpClient.get<Product>(baseUrl);
   }
   serchDateProduct(dateParams:Observable<Product>) {
-    const baseUrl = "http://localhost:4200/date=" + dateParams;
+    const baseUrl = "http://localhost:3000/date=" + dateParams;
     return this.httpClient.get<Product>(baseUrl);
+  }
+  getCat(){
+    const CatUrl = "http://localhost:3000/cat";
+    return this.httpClient.get<Cat>(CatUrl);
   }
 }
--- src/app/site-layout/sidebar/sidebar.component.ts
+++ src/app/site-layout/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/products/product.service';
+import { Cat } from '../cat';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  catag: any;
+  constructor(private productsService: ProductService) { }
 
   ngOnInit(): void {
+    this.productsService.getCat().subscribe(data=>{
+      this.catag = data;
+    })
   }
 
 }
